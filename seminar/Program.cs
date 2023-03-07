c713b756ce284ba102d3f336dd55f8465dcade87
Andrey <and.ermolov2014@yandex.ru>
2023-03-07 07:25:52 +0300

    Seminar ended

---
+++ seminar/Program.cs
@@ -0,0 +1,48 @@
+﻿//int num = -12;
+//double bigNum = -12.13;
+//bool isEven = true;
+//string word = "flower";
+//char symbol = '4';
+
+//int num1 = -12;
+//int num2 = 6;
+
+//Console.Write("My number is = " + num1 + " and " + num2);
+//Console.WriteLine($"My number is = {num1} and {num2}");
+
+//Console.Write("Input an integer number: ");
+//int num = Convert.ToInt32(Console.ReadLine()); 
+//Console.WriteLine(num);
+
+//Task 1.
+/*
+Console.Write("Input a  first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a  second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = num2 * num2;
+
+if(num1 == quad2)
+{
+    Console.Write("Yes");
+}
+else
+{
+    Console.Write("No");
+}
+*/
+
+//Task 2.
+
+/*Console.Write("Input a positive number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = -num;
+
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
